ca2c5ba857d3614bb0970746a645fd34036d9547
goldh0rse <klas.g.holmberg@gmail.com>
2022-03-21 11:30:49 +0100

    1.5 hours in

--- src/main.go
+++ src/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	goConf := "Go!-Conf"
@@ -11,26 +14,45 @@ func main() {
 	fmt.Printf("Total visitor capacity: %d\n", capacity)
 	fmt.Printf("Tickets available: %d\n", ticketsAvailable)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
+	// var bookings []string // Slice type
+	bookings := []string{} // Slice type
 
-	fmt.Print("Enter your first name?: ")
-	fmt.Scan(&firstName) // Scanning requires pointers in GoLang
+	// var bookings [capacity]string // Array type
 
-	fmt.Print("Enter your last name?: ")
-	fmt.Scan(&lastName) // Scanning requires pointers in GoLang
+	for {
 
-	fmt.Print("Enter your email?: ")
-	fmt.Scan(&email) // Scanning requires pointers in GoLang
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
 
-	fmt.Print("How many tickets do you want?: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter your first name?: ")
+		fmt.Scan(&firstName) // Scanning requires pointers in GoLang
 
-	ticketsAvailable -= uint8(userTickets)
+		fmt.Print("Enter your last name?: ")
+		fmt.Scan(&lastName) // Scanning requires pointers in GoLang
 
-	fmt.Printf("Thank You!! %v %v for booking %v tickets. You will receive confirmation over email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v!\n", ticketsAvailable, goConf)
+		fmt.Print("Enter your email?: ")
+		fmt.Scan(&email) // Scanning requires pointers in GoLang
 
+		fmt.Print("How many tickets do you want?: ")
+		fmt.Scan(&userTickets)
+
+		ticketsAvailable -= uint8(userTickets)
+
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName+" "+lastName)
+		fmt.Printf("Thank You!! %v %v for booking %v tickets. You will receive confirmation over email at %v\n", firstName, lastName, userTickets, email)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("Our bookings: %v\n", bookings)
+		fmt.Printf("First name of all bookings: %v\n", firstNames)
+	}
+
+	// fmt.Printf("%v tickets remaining for %v!\n", ticketsAvailable, goConf)
 }
